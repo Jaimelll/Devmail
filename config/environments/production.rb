6de8bd6afdcef6fbbdc9464bca3ff80b9f0e6c52
Jaimelll <jaduran0102@gmail.com>
2020-01-27 20:03:13 -0800

    falta conf cta google menos seg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,13 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
-  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
-
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.secret_key_base = ENV["SECRET_KEY_BASE"]
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
@@ -28,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -38,9 +33,6 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-
-  # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
@@ -62,22 +54,22 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "devmail_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "almacen_#{Rails.env}"
+  config.require_master_key = true 
 
   config.action_mailer.perform_caching = false
-  config.read_encrypted_secrets = true
-config.action_mailer.default_url_options = { :host => 
-"domain.com" }
-config.action_mailer.perform_deliveries = true
-config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options={host:"localhost:3000"}
 
-config.action_mailer.smtp_settings = {
- :address => "smtp.gmail.com",
- :port => 587,
- :user_name => "jaduran0102@gmail.com",
- :password => "vanessapaola",
- :enable_starttls_auto => true
-}
+  config.action_mailer.delivery_method= :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :authentication=> "plain",
+    :enable_starttls_auto=> true,
+    :user_name => "jaduran0102@gmail.com",
+    :password => "vanessapaola"    
+   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
